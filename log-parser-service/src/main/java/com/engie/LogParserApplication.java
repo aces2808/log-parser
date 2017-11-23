5bcdecaed5cb5c834e99fd6f30b31bfd93f3b531
aces28 <aces28@gmail.com>
2017-11-23 14:32:04 +0000

    -Added commons cli

--- log-parser-service/src/main/java/com/engie/LogParserApplication.java
+++ log-parser-service/src/main/java/com/engie/LogParserApplication.java
@@ -1,4 +1,20 @@
 package com.engie;
 
-public class LogParserApplication {
+import com.engie.tractebel.log.configuration.LogConfiguration;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@ImportAutoConfiguration(value = LogConfiguration.class)
+public class LogParserApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogParserApplication.class, args);
+    }
+
+    public void run(String... strings) throws Exception {
+
+    }
 }
---
+++ log-parser-service/src/main/java/com/engie/tractebel/log/configuration/LogConfiguration.java
@@ -0,0 +1,9 @@
+package com.engie.tractebel.log.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.engie.*")
+public class LogConfiguration {
+}
