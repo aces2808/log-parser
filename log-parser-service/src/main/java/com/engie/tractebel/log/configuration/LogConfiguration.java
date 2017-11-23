09fefaee85b403c189a44b5a1f3c0cc0967742e8
aces28 <aces28@gmail.com>
2017-11-23 14:39:44 +0000

    -Added gitignore

--- log-parser-service/src/main/java/com/engie/tractebel/log/configuration/LogConfiguration.java
+++ log-parser-service/src/main/java/com/engie/tractebel/log/configuration/LogConfiguration.java
@@ -1,9 +1,10 @@
 package com.engie.tractebel.log.configuration;
 
+import com.engie.tractebel.log.constants.LogParserContants;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.engie.*")
+@ComponentScan(basePackages = LogParserContants.BASE_PACKAGE)
 public class LogConfiguration {
 }
---
+++ log-parser-service/src/main/java/com/engie/tractebel/log/constants/LogParserContants.java
@@ -0,0 +1,9 @@
+package com.engie.tractebel.log.constants;
+
+public interface LogParserContants {
+
+    /**
+     * Base packages to be scannedF
+     */
+    String BASE_PACKAGE = "com.engie.*";
+}
