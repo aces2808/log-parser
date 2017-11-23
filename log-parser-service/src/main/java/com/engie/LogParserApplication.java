31e27afbb99669e35485be5c6c62ccf09e7d9070
aces28 <aces28@gmail.com>
2017-11-23 13:48:07 +0000

    -Clean pom file

---
+++ log-parser-service/src/main/java/com/engie/LogParserApplication.java
@@ -0,0 +1,4 @@
+package com.engie;
+
+public class LogParserApplication {
+}
---
+++ log-parser-service/src/main/java/com/engie/tractebel/log/service/impl/LogParserImpl.java
@@ -0,0 +1,11 @@
+package com.engie.tractebel.log.service.impl;
+
+import com.engie.tractebel.log.service.ILogParser;
+
+import java.util.Map;
+
+public class LogParserImpl implements ILogParser {
+    public Map<String, Long> parser() {
+        return null;
+    }
+}
